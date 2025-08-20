d105b87615f215d059f6bda68f97e6122c7ecbd3
Rita Vasconcelos <ritavasconcelos@Rita-Vasconcelos-Mac-ArcTouch-61A.local>
2025-08-19 22:11:41 -0400

    refactor

--- AIPresentation/AIPresentation/Order.swift
+++ AIPresentation/AIPresentation/Order.swift
@@ -10,10 +10,19 @@ import Foundation
 struct Order {
     let price: Double
     let quantity: Int
+    let taxRate: Double 
 }
 
 class OrderCalculator {
-    func calculateTotalPrice(for order: Order) -> Double {
+    func calculateSubtotal(for order: Order) -> Double {
         return order.price * Double(order.quantity)
+    }
+
+    func calculatetax(for order: Order) -> Double {
+        return calculateSubtotal(for: order) * order.taxRate
+    }
+
+    func calculateTotalPrice(for order: Order) -> Double {
+        return calculateSubtotal(for: order) + calculatetax(for: order)
     }
 }
