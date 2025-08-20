53e3a46d1dd09b4140f00743ba149531867b0558
Rita Vasconcelos <ritavasconcelos@Rita-Vasconcelos-Mac-ArcTouch-61A.local>
2025-08-19 21:13:27 -0400

    first commit

---
+++ AIPresentation/AIPresentation/AIPresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIPresentationApp.swift
+//  AIPresentation
+//
+//  Created by Rita Vasconcelos on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AIPresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIPresentation/AIPresentationTests/AIPresentationTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIPresentationTests.swift
+//  AIPresentationTests
+//
+//  Created by Rita Vasconcelos on 19/08/25.
+//
+
+import Testing
+
+struct AIPresentationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
